56ade8b6b1b749cae2ceb402a693943641e09fe7
owalker10 <36974651+owalker10@users.noreply.github.com>
2022-04-14 23:18:40 -0500

    more work on components and removed node_modules from git

--- routes/web.js
+++ routes/web.js
@@ -12,7 +12,13 @@ router.get('/profile', function(req, res, next) {
 
 // Renders the "example" page
 router.get('/example', function(req, res, next) {
-  res.render('pages/example');
+  res.render('pages/example', {foo:'bar'});
 });
+
+// send over HTML of a component, useful for rendering components with new data without reloading entire page
+router.post('/test', function(req, res, next) {
+  res.render('components/button', { size: 'small', type: 'primary', text: 'close', id: 'test'});
+});
+
 
 module.exports = router;
